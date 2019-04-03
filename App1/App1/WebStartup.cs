8e7f0d955223921490cf10f3c0fc0e509e0aff12
Jeremy Beavon <jeremybeavon@users.noreply.github.com>
2019-04-03 22:19:28 +1300

    check-in demo project

---
+++ App1/App1/WebStartup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using ProxyKit;
+using System.Collections.Generic;
+
+namespace App1
+{
+    public sealed class WebStartup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddProxy();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.RunProxy(async context =>
+            {
+                var response = await context
+                    .ForwardTo("https://integrity-betasite.fnfis.com")
+                    .Send();
+                response.Headers.Remove("Content-Security-Policy");
+                return response;
+            });
+        }
+    }
+}
+
---
+++ App1/WebApplication1/Startup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ProxyKit;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddProxy();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.RunProxy(async context =>
+            {
+                var response = await context
+                    .ForwardTo("https://integrity-betasite.fnfis.com")
+                    .Send();
+                response.Headers.Remove("Content-Security-Policy");
+                return response;
+            });
+        }
+    }
+}
